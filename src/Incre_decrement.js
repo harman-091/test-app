412363430c03725eb162f74085270fdcd911fbb7
harman-091 <harmanwarring091@gmail.com>
2026-02-24 03:51:47 -0800

    a

---
+++ src/Incre_decrement.js
@@ -0,0 +1,37 @@
+import React, {useState,useCallback} from "react";
+var funccount = new Set();
+const Incredecre = () => {
+    const [count,setCount] = useState(0);
+    const [number,setNumber] = useState(0);
+
+    const incrementCounter=useCallback(() => {
+        setCount(count + 1);
+    },[count]);
+    const decrementCounter=useCallback(() => {
+        setCount(count - 1);
+    },[count]);
+    
+    funccount.add(incrementCounter);
+    funccount.add(decrementCounter);
+
+    return (
+        <div style={{
+            display:"flex",
+            flexDirection:"column",
+            textAlign:"center",
+            justifyContent:"end",
+            margin:"auto",
+            marginTop:"20px",
+            width:"350px",
+            padding:"50px",
+            height:"300px",
+            fontSize:"20px",
+            boxShadow:"0px 2px 8px 4px grey",
+            borderRadius:"5px",
+        }}>
+            <h2 style={{color:"green"}}>GeeksforGeeks</h2>
+            <h4></h4>
+
+        </div>
+    )
+}
