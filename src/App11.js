4ff77f21463c36d56942c73f28e4568b47558c62
harman-091 <harmanwarring091@gmail.com>
2026-02-25 03:45:08 -0800

    a

---
+++ src/App11.js
@@ -0,0 +1,96 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Profile from './Profile';
+import Propp from './propp';
+import Buttonss from './Buttonss';
+import ConButton from './ConButton';
+import PrgBar from './PrgBar';
+import ColRow from './ColRow';
+import Cardd from './Cardd';
+import DropDownn from './DropDownn';
+import Listgroupp from './Listgroupp';
+import Alertt from './Alertt';
+import Navbar from './Navbar';
+import NameUpdate from './nameupdateuseState';
+import Precount from './precount';
+import NameChange from './namechange';
+import NumberList from './numberList';
+import HookCounterOne from './HookCounterr';
+import Calculator from './Calculator';
+import TodoList from './TodoList';
+import PChild from "./P_child";
+import Incredecre from './Incre_decrement';
+import UseREF from './UseREF';
+import SqNum from './SqNum';
+
+
+
+
+function App() {
+  return (
+    <div>
+        
+        <Navbar/>
+        <hr></hr>
+        <hr></hr>
+        <SqNum/>
+        <hr></hr>
+        <hr></hr>
+        <UseREF/>
+        <hr></hr>
+        <PChild/>
+        <hr></hr>
+        <Incredecre/>
+        <hr></hr>
+        <HookCounterOne/>
+        <hr></hr>
+        <hr></hr>
+        <NumberList/>
+        <hr></hr>
+        <hr></hr>
+        <NameChange/>
+        <hr></hr>
+        <hr></hr>
+        <Profile/>
+        <hr></hr>
+        <hr></hr>
+        <Precount/>
+        <hr></hr>
+        <hr></hr>
+        <NameUpdate/>
+        <hr></hr>
+        <hr></hr>
+        <Cardd/>
+        <hr></hr>
+        <hr></hr>
+        <DropDownn/>
+        <hr></hr>
+        <hr></hr>
+        <Listgroupp/>
+        <hr></hr>
+        <hr></hr>
+        <Alertt/>
+        <hr></hr>
+        <hr></hr>
+        <Propp name="John" ca="Superman"/>
+        <Propp name="Jane" ca="Batman"/>
+        <Propp name="Bob" ca="Wonder Woman"/>
+        <p>New data</p>
+        <Buttonss/>
+        <ConButton/>
+        <PrgBar/>
+        <hr></hr>
+        <hr></hr>
+        <Calculator/>
+        <TodoList/>
+        <hr></hr>
+        <hr></hr>
+        <ColRow/>
+        <hr></hr>
+        <hr></hr>
+        <p>Old data</p>
+    </div>
+  );
+}
+
+export default App;
